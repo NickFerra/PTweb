41971bda73542c8e1ac5c9c2ddafa660a545bb57
Nicholas Ferrari <Nick.ferrari@me.com>
2021-05-05 13:54:47 +0100

    top bar css

--- frontend/src/components/top_bar.js
+++ frontend/src/components/top_bar.js
@@ -1,11 +1,11 @@
-
+import "./css/top_bar.css"
 
 function Top_bar() {
-    return (
-        <div className="top_bar">
-            <div className="top_bar_text">NF Personal Training</div>
-        </div>
-    );
+  return (
+    <div className="top_bar">
+      <div className="top_bar_text">NF Personal Training</div>
+    </div>
+  );
 }
 
 export default Top_bar;
